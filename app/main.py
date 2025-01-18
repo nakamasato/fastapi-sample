4b0a84fbde3d8bf796c88c8628efd2ae68d93040
Yuji Tochio <yujitochio@users.noreply.github.com>
2025-01-18 17:21:19 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def get_db():
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World, Hello!!!"}
 
 
 @app.post("/users/", response_model=schemas.User)
